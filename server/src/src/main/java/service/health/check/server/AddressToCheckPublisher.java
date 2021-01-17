624f09df49d692cffa1895ada67cfdb60be3e77b
Kuba Pierewoj <kuba@pierewoj.com>
2021-01-17 19:08:09 +0100

    Add lombok to server

--- server/src/src/main/java/service/health/check/server/AddressToCheckPublisher.java
+++ server/src/src/main/java/service/health/check/server/AddressToCheckPublisher.java
@@ -5,6 +5,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.health.check.messages.AddressToCheck;
@@ -15,14 +16,14 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+@Slf4j
 public class AddressToCheckPublisher {
     public void publishAddressesForChecking(List<Address> addresses) throws IOException, TimeoutException {
-        Logger logger = LoggerFactory.getLogger(App.class);
         try (Connection connection = new ConnectionFactory().newConnection(com.rabbitmq.client.Address.parseAddresses(
                 Config.RABBITMQ_CONNECTION_ADDRESS))) {
             try (Channel channel = connection.createChannel()) {
                 channel.queueDeclare(Config.ADDRESSES_TO_CHECK_QUEUE, true, false, false, null);
-                logger.info("Got {} addresses", addresses.size());
+                log.info("Got {} addresses", addresses.size());
                 for (service.health.check.models.Address address : addresses) {
                     AddressToCheck addressToCheck = new AddressToCheck(address.getHost(),
                             address.getPort());
@@ -31,7 +32,7 @@ public class AddressToCheckPublisher {
                     channel.basicPublish("", Config.ADDRESSES_TO_CHECK_QUEUE,
                             MessageProperties.PERSISTENT_TEXT_PLAIN,
                             messageJson.getBytes());
-                    logger.info("Server - Message sent: " + messageJson);
+                    log.info("Server - Message sent: " + messageJson);
                 }
             }
         }
