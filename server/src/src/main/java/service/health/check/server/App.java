aa5a15c05a0a6f6ab0ead4f8f80b7886488dd321
Kuba Pierewoj <kuba@pierewoj.com>
2021-01-17 19:23:27 +0100

    Log exception in scheduler

--- server/src/src/main/java/service/health/check/server/App.java
+++ server/src/src/main/java/service/health/check/server/App.java
@@ -2,26 +2,30 @@ package service.health.check.server;
 
 import com.google.common.util.concurrent.AbstractScheduledService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class App {
 	public App() {
 	}
 
 	@AllArgsConstructor
+	@Slf4j
 	static class ScheduledExecutor extends AbstractScheduledService
 	{
 		private final Database db;
 		private final AddressToCheckPublisher pub;
 
 		@Override
-		protected void runOneIteration() throws Exception {
-			Database db = new Database();
-			AddressToCheckPublisher publisher = new AddressToCheckPublisher();
-			publisher.publishAddressesForChecking(db.getAllAddresses());
+		protected void runOneIteration() {
+			try {
+				Database db = new Database();
+				AddressToCheckPublisher publisher = new AddressToCheckPublisher();
+				publisher.publishAddressesForChecking(db.getAllAddresses());
+			} catch (Exception e) {
+				log.error("Exception thrown during run execution: {}", e.toString());
+			}
 		}
 
 		@Override
@@ -30,8 +34,7 @@ public class App {
 		}
 	}
 
-	public static void main(String[] args)
-			throws IOException, TimeoutException {
+	public static void main(String[] args) {
 		ScheduledExecutor executor = new ScheduledExecutor(new Database(), new AddressToCheckPublisher());
 		executor.startAsync();
 		executor.awaitTerminated();
